e24364dbda61d11ad4eeb6051b8b25f68b5086d0
LEE YOON SEOK <dmzld94@gmail.com>
2018-11-20 18:22:51 +0900

    3rd

--- app/src/main/java/com/kong/armonia/armonia/MainActivity.java
+++ app/src/main/java/com/kong/armonia/armonia/MainActivity.java
@@ -21,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         tv.setText(stringFromJNI());
     }
 
+
+    //hi
+    
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
